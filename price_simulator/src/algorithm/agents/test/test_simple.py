af127f3cb0cea41bb2d4673839201a9312f56e79
mesjou <57187097+mesjou@users.noreply.github.com>
2021-11-02 13:22:01 +0100

    fixed tests

--- price_simulator/src/algorithm/agents/test/test_simple.py
+++ price_simulator/src/algorithm/agents/test/test_simple.py
@@ -30,5 +30,5 @@ def test_play_price():
     assert agent.play_price((4.0, 4.0, 2.0), [1.0, 2.0, 3.0, 4.0], 0, 0) == 4.0
     assert agent.play_price((2.0, 3.0, 1.0), [1.0, 2.0, 3.0, 4.0], 0, 0) == 2.0     
     assert agent.play_price((4.0, 4.0, 4.0), [1.0, 2.0, 3.0, 4.0], 0, 0) == 4.0
-    assert agent.play_price((4.0, 1.0, 3.0), [1.0, 2.0, 3.0, 4.0], 0, 0) == 10
+    assert agent.play_price((4.0, 1.0, 3.0), [1.0, 2.0, 3.0, 4.0], 0, 0) == 1.0
     assert agent.play_price((3.0, 4.0, 2.0), [1.0, 2.0, 3.0, 4.0], 0, 0) == 3.0
